437f2dd4a464a7882d8f1789cf66899d37f5e4b4
samkunthida <kunthidakhlongkhlaew@gmail.com>
2025-12-02 16:27:58 +0700

    Update Vite config for GitHub Pages deployment and refine workflow steps

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ถ้าหน้า Github Pages ของคุณคือ https://samkunthida.github.io/test-app/
+  // ให้ตั้งเป็น '/test-app/' หรือ ใช้ './' หากต้องการพาธแบบ relative
+  base: '/test-app/',
 })
